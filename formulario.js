44a120e1534c6053affb45d0cc7378d499597635
jafh1906 <juanfrancoclasesvirtuales@gmail.com>
2025-05-18 10:49:33 -0500

    Laboratorio 3 actualizacion

--- formulario.js
+++ formulario.js
@@ -4,13 +4,11 @@ form.addEventListener("submit", function (event) {
 event.preventDefault()
     
 const nombre = form.nombre.value.trim(); 
-const fecha = form.fecha.value.trim(); 
 const email = form.email.value.trim(); 
 const celular = form.celular.value.trim(); 
-const mensaje = form.mensaje.value.trim(); 
-const pais = form.pais.value.trim();  
+const mensaje = form.mensaje.value.trim();   
 
-if((nombre != null) && (fecha != null)  && (email != null) && (celular != null) && (mensaje != null) && (pais != null)){
+if((nombre != null) && (email != null) && (celular != null) && (mensaje != null)){
     alert("¡Bienvenido/a, "+ nombre + "!Gracias por compeltar el formulario!");
  } else {
     alert("Por favor, completa todos los campos.");
